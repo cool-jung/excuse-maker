29a79b1ef122755f91b9eb1942f66145ca581a7f
siwon <coolqha@naver.com>
2022-04-03 01:57:34 +0900

    feat: 클립보드 복사 기능 추가

--- excuse-maker-cra/src/components/SelectPage.js
+++ excuse-maker-cra/src/components/SelectPage.js
@@ -2,11 +2,25 @@ import React from "react";
 import {Link} from "react-router-dom";
 
 function SelectPage(){
+    
+
+    function copyToClipBoard(){
+        const content = document.getElementById('excuseText').innerHTML;
+    
+        navigator.clipboard.writeText(content)
+            .then(() => {
+            console.log("Text copied to clipboard...")
+        })
+            .catch(err => {
+            console.log('Something went wrong', err);
+        })
+    }
+
     return(
         <div>
-            <h1>변명</h1>
+            <h1 id="excuseText">변명</h1>
             <button>다시 선택</button>
-            <button>변명 확정</button>
+            <button onClick={copyToClipBoard}>변명 확정</button>
             <p><Link to="../category">뒤로가기</Link></p>
         </div>
     );
