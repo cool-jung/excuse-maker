2f8470d7e72ce453ca164a5df94f276adf7a6bac
siwon <coolqha@naver.com>
2022-04-04 23:35:18 +0900

    feat : print random

--- excuse-maker-cra/src/components/CategoryPage.js
+++ excuse-maker-cra/src/components/CategoryPage.js
@@ -1,11 +1,22 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import data from "../db/data.json";
 
-function CategoryPage(){    
+function CategoryPage(){
+    function randomValueExcuseArray(){
+        const random = Math.floor(Math.random() * 10);
+        
+        const excuseText = document.querySelector("#excuseText").innerHTML=data.time[random];
+        // return console.log(excuseText);
+        return console.log(data.time[random]);
+
+    }
+    
+    
     return(
         <div>
             <ul>
-                <li><button><Link to="../select">시간 약속</Link></button></li>
+                <li><button onClick={randomValueExcuseArray}><Link to="../select">시간 약속</Link></button></li>
                 <li><button><Link to="../select">일정 약속</Link></button></li>
             </ul>
         </div>
